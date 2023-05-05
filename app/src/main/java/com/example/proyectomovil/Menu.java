aa196d092faf6040a184ae9438f30134a15a96ae
William Castro <wcastroh07@gmail.com>
2023-05-05 17:31:10 -0500

    Menu modificado

--- app/src/main/java/com/example/proyectomovil/Menu.java
+++ app/src/main/java/com/example/proyectomovil/Menu.java
@@ -4,6 +4,7 @@ package com.example.proyectomovil;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Menu extends AppCompatActivity {
 
@@ -11,5 +12,9 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void salir(View view) {
+        onBackPressed();
     }
 }
