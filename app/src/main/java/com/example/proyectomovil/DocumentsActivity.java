b842b60fb57968c1b3ba7ed970f5bae4b57eb8f6
mauricio <mauricio_s7@outlook.com>
2023-06-10 17:21:46 -0500

    setup activities

--- app/src/main/java/com/example/proyectomovil/DocumentsActivity.java
+++ app/src/main/java/com/example/proyectomovil/DocumentsActivity.java
@@ -1,20 +1,14 @@
-
 package com.example.proyectomovil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class DocumentsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void salir(View view) {
-        onBackPressed();
+        setContentView(R.layout.activity_documents);
     }
 }
--- app/src/main/java/com/example/proyectomovil/RegisterActivity.java
+++ app/src/main/java/com/example/proyectomovil/RegisterActivity.java
@@ -1,20 +1,14 @@
-
 package com.example.proyectomovil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class Menu extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
-    }
-
-    public void salir(View view) {
-        onBackPressed();
+        setContentView(R.layout.activity_register);
     }
 }
